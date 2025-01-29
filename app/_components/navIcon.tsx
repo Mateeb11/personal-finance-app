1f87feaedea77ea8460ba801f66e0d166c3085b4
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2025-01-29 15:40:34 +0300

    style active navbar link

--- app/_components/navIcon.tsx
+++ app/_components/navIcon.tsx
@@ -10,11 +10,15 @@ export default function NavIcon({ src, alt, href }: any) {
 
   return (
     <div
-      className={`grid place-items-center min-w-16 min-h-12 ${
-        path.startsWith(href) ? classes.active : undefined
-      }`}
+      className={`grid place-items-center min-w-16 min-h-11 ${
+        classes.roundBorders
+      } ${path.startsWith(href) ? classes.active : undefined}`}
     >
-      <Image src={src} width={24} height={24} alt={alt}></Image>
+      <Image
+        src={src}
+        alt={alt}
+        className={path.startsWith(href) ? classes.activeFill : undefined}
+      ></Image>
     </div>
   );
 }
