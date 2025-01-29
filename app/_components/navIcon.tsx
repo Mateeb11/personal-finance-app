29f5073aa684a5d054ea58319bf59a84ecbad22d
Mateeb Alharbi <63017846+Mateeb11@users.noreply.github.com>
2025-01-29 11:59:59 +0300

    style nav bar

--- app/_components/navIcon.tsx
+++ app/_components/navIcon.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function NavIcon({ src, alt }: any) {
   return (
-    <>
+    <div className="grid place-items-center min-w-16 min-h-12">
       <Image src={src} width={24} height={24} alt={alt}></Image>
-    </>
+    </div>
   );
 }
